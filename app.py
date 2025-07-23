d867328b2c361dfc9598f4e11eb9ce93d9106d65
unknown <finozhenkov.alexander@gmail.com>
2025-07-23 12:50:19 +0300

    added better routing

--- app.py
+++ app.py
@@ -10,11 +10,20 @@ from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/token")
+exception_details = {
+    "invalid_token": "Неверный токен",
+    "expired_token": "Время сессии истекло",
+    "invalid_login_password": "Неверный логин или пароль",
+    "user_not_found": "Пользователь не найден",
+    "user_already_exists": "Пользователь с таким логином уже существует",
+    "insufficient_permissions": "Нет прав доступа"
+}
+
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-EXPIRATION_TIME = timedelta(minutes = 5)
+ALGORITHM = "HS256"
+EXPIRATION_TIME = timedelta(minutes = 1)
 
 
 class Token:
@@ -37,6 +46,7 @@ class Token:
         data["expiration"] = (self.__expiration).isoformat()
         return jwt.encode(data, SECRET_KEY, algorithm = ALGORITHM)
     
+    @staticmethod
     def decode(token):
         try: 
             data = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -54,12 +64,14 @@ class User:
     def is_pw_correct(self, password: str):
         return bcrypt.checkpw(password.encode("utf-8"), self.hashed_password.encode("utf-8"))
 
-    def get_by_id(id: str):
+    @staticmethod
+    def get_by_id(id: str, users: list):
         for user in users: 
             if user.id == id: return user
         return None
     
-    def get_by_login(login: str):
+    @staticmethod
+    def get_by_login(login: str, users: list):
         for user in users: 
             if user.login == login: return user
         return None
@@ -73,33 +85,33 @@ class User:
         self.promdate = data["promdate"]
 
 
-def save_db(users):
+def save_db(users: list):
     users_json = [vars(user) for user in users]
     with open("db.json", "w", encoding = "utf-8") as json_file: json.dump(users_json, json_file, ensure_ascii = False, indent = 4)
 
 def load_db():
     with open("db.json", encoding="utf-8") as json_file: return [User(data) for data in json.loads(json_file.read())]
 
-
 users = load_db()
+
 
 app = FastAPI()
 
-@app.post("/home")
+@app.post("/")
 def get_current_user(token: str = Depends(oauth2_scheme)):
     token = Token.decode(token)
-    if not token: raise HTTPException(status_code = 400, detail = "Invalid token")
-    if token.is_expired(): raise HTTPException(status_code = 400, detail = "Expired token")
+    if not token: raise HTTPException(status_code = 400, detail = exception_details["invalid_token"])
+    if token.is_expired(): raise HTTPException(status_code = 400, detail = exception_details["expired_token"])
 
-    user = User.get_by_id(token.user_id)
-    if not user: raise HTTPException(status_code = 400, detail = "User not found")
+    user = User.get_by_id(token.user_id, users)
+    if not user: raise HTTPException(status_code = 400, detail = exception_details["user_not_found"])
 
     return user
 
 @app.post("/login")
 def auth_user(data = Body()):
-    user = User.get_by_login(data["login"])
-    if not (user and user.is_pw_correct(data["password"])): raise HTTPException(status_code = 400, detail = "Incorrect username or password")
+    user = User.get_by_login(data["login"], users)
+    if not (user and user.is_pw_correct(data["password"])): raise HTTPException(status_code = 400, detail = exception_details["invalid_login_password"])
     token = Token(user.id).encode()
     return {"access_token": token, "token_type": "bearer"}
 
@@ -108,20 +120,20 @@ def auth_user(data = Body()):
 
 @app.get("/users")
 def get_users(current_user = Depends(get_current_user)):
-    if current_user.login != "admin": raise HTTPException(status_code = 403, detail = "Insufficient permissions")
+    if current_user.login != "admin": raise HTTPException(status_code = 403, detail = exception_details["insufficient_permissions"])
     return users
 
 @app.get("/users/{id}")
 def get_user(id, current_user = Depends(get_current_user)):
-    if current_user.login != "admin": raise HTTPException(status_code = 403, detail = "Insufficient permissions")
-    user = User.get_by_id(id)
-    if user == None: raise HTTPException(status_code = 404, detail = "User not found")
+    if current_user.login != "admin": raise HTTPException(status_code = 403, detail = exception_details["insufficient_permissions"])
+    user = User.get_by_id(id, users)
+    if user == None: raise HTTPException(status_code = 404, detail = exception_details["user_not_found"])
     return user
 
 @app.post("/users")
 def create_user(data = Body(), current_user = Depends(get_current_user)):
-    if current_user.login != "admin": raise HTTPException(status_code = 403, detail = "Insufficient permissions")
-    if User.get_by_login(data["login"]): raise HTTPException(status_code = 405, detail = "User with this login already exisrs")
+    if current_user.login != "admin": raise HTTPException(status_code = 403, detail = exception_details["insufficient_permissions"])
+    if User.get_by_login(data["login"], users): raise HTTPException(status_code = 405, detail = exception_details["user_already_exists"])
     user = User(data)
     users.append(user)
     save_db(users)
@@ -129,24 +141,28 @@ def create_user(data = Body(), current_user = Depends(get_current_user)):
 
 @app.put("/users")
 def edit_user(data = Body(), current_user = Depends(get_current_user)):
-    if current_user.login != "admin": raise HTTPException(status_code = 403, detail = "Insufficient permissions")
-    user = User.get_by_id(data["id"])
-    if user == None: raise HTTPException(status_code = 404, detail = "User not found")
+    if current_user.login != "admin": raise HTTPException(status_code = 403, detail = exception_details["insufficient_permissions"])
+    user = User.get_by_id(data["id"], users)
+    if user == None: raise HTTPException(status_code = 404, detail = exception_details["user_not_found"])
     user.fill_props(data)
     save_db(users)
     return user
 
 @app.delete("/users/{id}")
 def delete_user(id, current_user = Depends(get_current_user)):
-    if current_user.login != "admin": raise HTTPException(status_code = 403, detail = "Insufficient permissions")
-    user = User.get_by_id(id)
-    if user == None: raise HTTPException(status_code = 404, detail = "User not found")
+    if current_user.login != "admin": raise HTTPException(status_code = 403, detail = exception_details["insufficient_permissions"])
+    user = User.get_by_id(id, users)
+    if user == None: raise HTTPException(status_code = 404, detail = exception_details["user_not_found"])
     users.remove(user)
     save_db(users)
     return user
 
 
 #Загрузка страниц
+
+@app.get("/")
+def root_page():
+    return FileResponse("pages/root.html")
 
 @app.get("/login")
 def login_page():
